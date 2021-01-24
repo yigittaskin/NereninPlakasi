fea260de563539880ce1b9db406f797e74d1df54
Yiğit Taşkın <43133579+yigittaskin@users.noreply.github.com>
2021-01-24 18:43:42 +0300

    Nerenin Plakası ?
    Girilen Plaka koduna göre şehri veren C# Form Uygulaması

---
+++ NereninPlakasi/NereninPlakasi/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace NereninPlakasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 137);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Plaka No:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(315, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 32);
+            this.label2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(115, 181);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Plaka Bul";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(197, 134);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(46, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(-3, 277);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(624, 33);
+            this.panel1.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(230, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "developer by <YGT>";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe Print", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Maroon;
+            this.label4.Location = new System.Drawing.Point(164, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(276, 43);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "NERENİN PLAKASI ?";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(471, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(85, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(46, 6);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(79, 69);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(616, 310);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe MDL2 Assets", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ NereninPlakasi/NereninPlakasi/Form1.cs
@@ -0,0 +1,280 @@
+﻿//YGT
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NereninPlakasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int plaka = Convert.ToInt32(textBox1.Text);
+
+            switch (plaka)
+            {
+                case 1:
+                    label2.Text = "Adana";
+                    break;
+                case 2:
+                    label2.Text = "Adıyaman";
+                    break;
+                case 3:
+                    label2.Text = "Afyon";
+                    break;
+                case 4:
+                    label2.Text = "Ağrı";
+                    break;
+                case 5:
+                    label2.Text = "Amasya";
+                    break;
+                case 6:
+                    label2.Text = "Ankara";
+                    break;
+                case 7:
+                    label2.Text = "Antalya";
+                    break;
+                case 8:
+                    label2.Text = "Artvin";
+                    break;
+                case 9:
+                    label2.Text = "Aydın";
+                    break;
+                case 10:
+                    label2.Text = "Balıkesir";
+                    break;
+                case 11:
+                    label2.Text = "Bilecik";
+                    break;
+                case 12:
+                    label2.Text = "Bingöl";
+                    break;
+                case 13:
+                    label2.Text = "Bitlis";
+                    break;
+                case 14:
+                    label2.Text = "Bolu";
+                    break;
+                case 15:
+                    label2.Text = "Burdur";
+                    break;
+                case 16:
+                    label2.Text = "Bursa";
+                    break;
+                case 17:
+                    label2.Text = "Çanakkale";
+                    break;
+                case 18:
+                    label2.Text = "Çankırı";
+                    break;
+                case 19:
+                    label2.Text = "Çorum";
+                    break;
+                case 20:
+                    label2.Text = "Denizli";
+                    break;
+                case 21:
+                    label2.Text = "Diyarbakır";
+                    break;
+                case 22:
+                    label2.Text = "Edirne";
+                    break;
+                case 23:
+                    label2.Text = "Elazığ";
+                    break;
+                case 24:
+                    label2.Text = "Erzincan";
+                    break;
+                case 25:
+                    label2.Text = "Erzurum";
+                    break;
+                case 26:
+                    label2.Text = "Eskişehir";
+                    break;
+                case 27:
+                    label2.Text = "Gaziantep";
+                    break;
+                case 28:
+                    label2.Text = "Giresun";
+                    break;
+                case 29:
+                    label2.Text = "Gümüşhane";
+                    break;
+                case 30:
+                    label2.Text = "Hakkari";
+                    break;
+                case 31:
+                    label2.Text = "Hatay";
+                    break;
+                case 32:
+                    label2.Text = "Isparta";
+                    break;
+                case 33:
+                    label2.Text = "MERSİN";
+                    break;
+                case 34:
+                    label2.Text = "İstanbul";
+                    break;
+                case 35:
+                    label2.Text = "İzmir";
+                    break;
+                case 36:
+                    label2.Text = "Kars";
+                    break;
+                case 37:
+                    label2.Text = "Kastamonu";
+                    break;
+                case 38:
+                    label2.Text = "Kayseri";
+                    break;
+                case 39:
+                    label2.Text = "Kırklareli";
+                    break;
+                case 40:
+                    label2.Text = "Kırşehir";
+                    break;
+                case 41:
+                    label2.Text = "KOCAELİ";
+                    break;
+                case 42:
+                    label2.Text = "Konya";
+                    break;
+                case 43:
+                    label2.Text = "Kütahya";
+                    break;
+                case 44:
+                    label2.Text = "Malatya";
+                    break;
+                case 45:
+                    label2.Text = "Manisa";
+                    break;
+                case 46:
+                    label2.Text = "K.Maraş";
+                    break;
+                case 47:
+                    label2.Text = "Mardin";
+                    break;
+                case 48:
+                    label2.Text = "Muğla";
+                    break;
+                case 49:
+                    label2.Text = "Muş";
+                    break;
+                case 50:
+                    label2.Text = "Nevşehir";
+                    break;
+                case 51:
+                    label2.Text = "Niğde";
+                    break;
+                case 52:
+                    label2.Text = "Ordu";
+                    break;
+                case 53:
+                    label2.Text = "Rize";
+                    break;
+                case 54:
+                    label2.Text = "Sakarya";
+                    break;
+                case 55:
+                    label2.Text = "Samsun";
+                    break;
+                case 56:
+                    label2.Text = "Siirt";
+                    break;
+                case 57:
+                    label2.Text = "Sinop";
+                    break;
+                case 58:
+                    label2.Text = "Sivas";
+                    break;
+                case 59:
+                    label2.Text = "Tekirdağ";
+                    break;
+                case 60:
+                    label2.Text = "Tokat";
+                    break;
+                case 61:
+                    label2.Text = "Trabzon";
+                    break;
+                case 62:
+                    label2.Text = "Tunceli";
+                    break;
+                case 63:
+                    label2.Text = "Şanlıurfa";
+                    break;
+                case 64:
+                    label2.Text = "Uşak";
+                    break;
+                case 65:
+                    label2.Text = "Van";
+                    break;
+                case 66:
+                    label2.Text = "Yozgat";
+                    break;
+                case 67:
+                    label2.Text = "Zonguldak";
+                    break;
+                case 68:
+                    label2.Text = "Aksaray";
+                    break;
+                case 69:
+                    label2.Text = "Bayburt";
+                    break;
+                case 70:
+                    label2.Text = "Karaman";
+                    break;
+                case 71:
+                    label2.Text = "Kırıkkale";
+                    break;
+                case 72:
+                    label2.Text = "Batman";
+                    break;
+                case 73:
+                    label2.Text = "Şırnak";
+                    break;
+                case 74:
+                    label2.Text = "Bartın";
+                    break;
+                case 75:
+                    label2.Text = "Ardahan";
+                    break;
+                case 76:
+                    label2.Text = "Iğdır";
+                    break;
+                case 77:
+                    label2.Text = "Yalova";
+                    break;
+                case 78:
+                    label2.Text = "Karabük";
+                    break;
+                case 79:
+                    label2.Text = "Kilis";
+                    break;
+                case 80:
+                    label2.Text = "Osmaniye";
+                    break;
+                case 81:
+                    label2.Text = "Düzce";
+                    break;
+                case 82:
+                    label2.Text = "Musul";
+                    break;
+                case 83:label2.Text = "Kerkük";
+                    break;
+                default:label2.Text = "Geçersiz plaka kodu girdiniz...";
+                    break;
+            }
+        }
+    }
+}
